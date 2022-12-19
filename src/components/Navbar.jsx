2cbcb21063a382854c077735f6eb0b2881ba8734
SofyGeisel <sofiafigueroageisel@gmail.com>
2022-12-19 15:34:36 -0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
+  const setActiveClass = ({ isActive }) => (isActive ? "active" : "noactive");
   return (
-    <div>Navbar</div>
-  )
+    <>
+      <div className="pizzaheader">
+        <div className="nav-container">
+          <h5>🍕Pizzeria Mamma Mia</h5>
+        </div>
+        <div className="subnav-container">
+          <h1>¡Pizzería Mamma Mia!</h1>
+          <h4>¡Tenemos las mejores pizzas que podrás encontrar!</h4>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
